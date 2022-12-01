43b24a57239bb87bf9fc5baf2ee9775c93460ea4
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2022-12-01 21:58:35 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//connection string (bağlantılar) burada tanımlanır.
+
+namespace DataAccessLayer.Concrete.Context
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-BBABLM7P;Database=CoreBlogDB;Integrated Security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
+
+//Migration:Amacı, Codefirst kullanarak tablo oluşturmak. 
