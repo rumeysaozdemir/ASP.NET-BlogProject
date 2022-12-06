893629ea6205700225bce10c3b4d1235077cf768
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2022-12-07 00:56:27 +0300

    07.12.2022

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -1,24 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-	public class CommentController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class CommentController : Controller
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public PartialViewResult PartialAddComment() 
-		{ 
-			return PartialView();
-		}
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
 
-		public PartialViewResult CommentListByBlog()
-		{
-			return PartialView();
-		}
+        public PartialViewResult CommentListByBlog(int id)
+        {
+            var values = cm.GetList(id);
+            return PartialView(values);
+        }
 
 
-	}
+    }
 }
